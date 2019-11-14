c3c820a5b69bb105c03d5fee758b8b8afde7dea1
MichalR <mrudny@gmail.com>
2019-11-14 13:45:02 +0100

    done w/o styling

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,16 +1,47 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+
+hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static(path.join(__dirname, 'public')));
 
+let beersArray;
+punkAPI.getBeers()
+  .then(beers => {
+    beersArray = beers;
+    })
+  .catch(error => {
+    console.log(error)
+  })
 
+app.get('/beers', (req, res, next) => {
+  res.render('beers', {
+    beerList: beersArray
+  });
+});
+
+let randomBeer;
+
+punkAPI.getRandom()
+  .then(beers => {
+    randomBeer = beers;
+    console.log(randomBeer);
+  })
+  .catch(error => {
+    console.log(error)
+  });
+
+app.get('/randomBeer', (req, res, next) => {
+  res.render('randomBeer', {
+    random: randomBeer
+  });
+});
 
 app.get('/', (req, res, next) => {
   res.render('index');
@@ -18,4 +49,7 @@ app.get('/', (req, res, next) => {
 
 
 
-app.listen(3000);
+
+app.listen(3000, () => {
+  console.log('Server running!');
+});
